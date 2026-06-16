a9f8efc9e6f7ca9a8c0eea03ffca8599e37a9e50
inakigarciaigm-svg <inakigarcia.igm@gmail.com>
2026-06-17 01:08:20 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'mundial2026-v3';
+const ASSETS = [
+  '/Mundial-/',
+  '/Mundial-/index.html',
+  '/Mundial-/manifest.json',
+  '/Mundial-/icons/icon-192.png',
+  '/Mundial-/icons/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
